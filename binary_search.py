a4dff9a40e2595e0480b44377edd8b4e3bb853e7
QambarAli1 <qamberali4904@gmail.com>
2021-12-13 00:59:39 +0500

    binary serach algorithm

--- binary_search.py
+++ binary_search.py
@@ -2,16 +2,16 @@ list = [10,20,30,40,50,60,70,80,90,100]
 searchNum = int(input("Enter number to search : "))
 start = 0
 end = len(list)-1
-flag=True
-while start <= end and flag==True:
+flag=False
+while start <= end and flag==False:
     midpoint = (start+end)//2
     if searchNum == list[midpoint]:
         print(f'Number is found at index {midpoint}')
-        flag=False
+        flag=True
     else:
         if list[midpoint] <= searchNum:
             start= midpoint+1
         if list[midpoint] >= searchNum:
             end= midpoint-1
-if flag==True:
+if flag==False:
     print('Number not found')
